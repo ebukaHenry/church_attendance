94f41c3cf7d7f1b5400bea17c6ed7f84dc8db3e8
Henrysuccess <okaforebuka47@gmail.com>
2026-05-23 13:24:27 +0100

    Fix: added Procfile

--- app.py
+++ app.py
@@ -111,4 +111,5 @@ def settings():
 
 if __name__ == '__main__':
     os.makedirs('static', exist_ok=True)
-    app.run(debug=True)
+    # For local development only
+    app.run(host='0.0.0.0', port=5000, debug=True)
